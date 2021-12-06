c983ccf014d3b6bbcb25cd38b2fc91850ec56a7d
Owen Sullivan <multiojuice@gmail.com>
2021-12-05 21:40:21 -0500

    Everything is working

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 
+let options = {}
 
 $(document).ready(function() {
+	$( "#uploader" ).change(function(event) {
+		console.warn("CHANGE", event.target.files[0]);
+		const formData = new FormData();
+		formData.append("file", event.target.files[0]);
+		options = {
+		method: "POST",
+		body: formData,
+		};
+	  });
+
 	$( "#transform-button" ).click(function() {
-	  alert( "Handler for .click() called." );
+		console.warn("upload", options);
+		fetch("http://127.0.0.1:3000/upload", options)
+		.then((response) => {
+		return response.json();
+		})
+		.then((data) => {
+			if(data.url) {
+				$("#img-init").prop("src", data.url);
+			}
+		})
+		.catch((error) => {
+		console.log(error);
+		alert("There was an error uploading the file")
+		});
+
 	});
 });
