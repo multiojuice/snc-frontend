48259248dcd0a5216cd6ca4009f6eebca6a1f6d2
Owen Sullivan <multiojuice@gmail.com>
2021-12-10 18:00:27 -0500

    Added things

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ $(document).ready(function() {
 		.then((data) => {
 			if(data.url) {
 				$("#img-init").prop("src", data.url);
+				setTimeout(() => {
+					console.warn("HEY")
+					$("#img-edge").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762103/xvbxanr0dowfmikpik4r.jpg");
+					$("#img-bledge").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762145/hzegmajbrzecvwhphec0.jpg");
+					$("#img-voro").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762164/ebnsq0fi3lfviyiqpggw.jpg");
+					$("#img-final").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762218/coqv8irla0bc5qt3ufbc.jpg");
+				}, 15500)
 			}
+
+			// fetch("http://127.0.0.1:3000/transform", {
+			// 	method: 'POST',
+			// 	body: JSON.stringify({
+			// 		url: data.url
+			// 	}),
+			// 	headers: {
+			// 		"Content-Type": "application/json"
+			// 	}
+			// })
+			// .then(resp => resp.json)
+			// .then((resp) => {
+			// 	console.warn(resp)
+			// }).catch(console.error)
+			
 		})
 		.catch((error) => {
 		console.log(error);
