de7ff68c2393072c9629dc32e1d6bb7ce3f03e7f
Owen Sullivan <multiojuice@gmail.com>
2021-12-11 17:30:37 -0500

    Fixed everything

--- index.js
+++ index.js
@@ -20,37 +20,35 @@ $(document).ready(function() {
 
 	$( "#transform-button" ).click(function() {
 		console.warn("upload", options);
+		$("#transform-button").prop('disabled', true);
 		fetch("http://styx.cs.rit.edu:3000/upload", options)
 		.then((response) => {
 		return response.json();
 		})
 		.then((data) => {
 			if(data.url) {
+				$("#loader").removeClass("hidden");
 				$("#img-init").prop("src", data.url);
-				setTimeout(() => {
-					console.warn("HEY")
-					$("#img-edge").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762103/xvbxanr0dowfmikpik4r.jpg");
-					$("#img-bledge").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762145/hzegmajbrzecvwhphec0.jpg");
-					$("#img-voro").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762164/ebnsq0fi3lfviyiqpggw.jpg");
-					$("#img-final").prop("src", "http://res.cloudinary.com/multiojuice/image/upload/v1638762218/coqv8irla0bc5qt3ufbc.jpg");
-				}, 15500)
+				fetch("http://styx.cs.rit.edu:3000/transform", {
+					method: 'POST',
+					body: JSON.stringify({
+						url: data.url,
+						panes: panes
+					}),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+				.then(resp => resp.json())
+				.then((resp) => {
+					console.warn(resp)
+					$("#loader").addClass("hidden");
+					$("#img-edge").prop("src", resp.edges);
+					$("#img-bledge").prop("src", resp.bledges);
+					$("#img-voro").prop("src", resp.voro);
+					$("#img-final").prop("src", resp.final);
+				}).catch(console.error)
 			}
-
-			fetch("http://127.0.0.1:3000/transform", {
-				method: 'POST',
-				body: JSON.stringify({
-					url: data.url,
-					panes: panes
-				}),
-				headers: {
-					"Content-Type": "application/json"
-				}
-			})
-			.then(resp => resp.json)
-			.then((resp) => {
-				console.warn(resp)
-			}).catch(console.error)
-			
 		})
 		.catch((error) => {
 		console.log(error);
